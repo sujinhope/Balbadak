d284b0e5e1a8d8eef5e1a242ccc853557250f76f
SOOJIN <soojin4964@gmail.com>
2020-06-11 13:05:37 +0900

    del port num

--- front-app/src/apis/apis.js
+++ front-app/src/apis/apis.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const apis = axios.create({
     // baseURL: 'http://www.balbadack.com/'
-    baseURL: 'https://www.balbadack.com:8399/api/'
+    baseURL: 'https://www.balbadack.com/api/'
     // baseURL: 'http://localhost:8399/'
     // baseURL:'http://192.168.1.242:7888/'
 });
